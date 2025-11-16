62639588f7b5bce855c2d9ae7811edfeb6dfa3a0
ArnabSaga <arnabdey15091@gmail.com>
2025-11-17 02:01:44 +0600

    Problem - 2 solved

--- solution.ts
+++ solution.ts
@@ -1,29 +1,25 @@
 //? Problem - 1
 
 const formatValue = (input: string | number | boolean) => {
-    if (typeof input === "string") {
-        return input.toUpperCase();
-    } else if (typeof input === "number") {
-        return input * 10;
-    } else if (typeof input === "boolean") {
-        return !input;
-    } else {
-        return undefined;
-    }
+  if (typeof input === "string") {
+    return input.toUpperCase();
+  } else if (typeof input === "number") {
+    return input * 10;
+  } else if (typeof input === "boolean") {
+    return !input;
+  } else {
+    return undefined;
+  }
 };
 
-/*
-//* gpt show me the overload version for problem-1. This only for myself improvement. 
-function formatValue(input: string): string;
-function formatValue(input: number): number;
-function formatValue(input: boolean): boolean;
-
-function formatValue(input: string | number | boolean) {
-    if (typeof input === "string") return input.toUpperCase();
-    if (typeof input === "number") return input * 10;
-    if (typeof input === "boolean") return !input;
-}
-*/
-
 //? Problem - 2
+const getLength = (length: string | any[]): number => {
+  if (typeof length === "string") {
+    return length.length;
+  } else if (Array.isArray(length)) {
+    return length.length;
+  } else {
+    throw new Error(`Give me valid input for counting length`);
+  }
+};
 
