7f0ba277ab6098020f813c8047b0b68eb6544220
ArnabSaga <arnabdey15091@gmail.com>
2025-11-17 02:19:18 +0600

    Problem - 3 solved with mashi mar style

--- solution.ts
+++ solution.ts
@@ -35,9 +35,12 @@ class Person {
   }
 
   getDetails() {
-    return `Name: ${this.name}, Age: ${this.age}`;
+    return `'Name: ${this.name}, Age: ${this.age}';`; //! :) with bugs mar style
   }
 }
 
 const person1 = new Person("John Doe", 30);
 console.log(person1.getDetails());
+
+const person2 = new Person("Alice", 25);
+console.log(person2.getDetails());
