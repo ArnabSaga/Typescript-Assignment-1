330ab72e3a45f7981b4970cc05037d72896c084a
ArnabSaga <arnabdey15091@gmail.com>
2025-11-17 01:17:16 +0600

    Solve the problem-1

---
+++ solution.ts
@@ -0,0 +1,24 @@
+//? Problem - 1
+
+const formatValue = (input: string | number | boolean) => {
+    if (typeof input === "string") {
+        return input.toUpperCase();
+    } else if (typeof input === "number") {
+        return input * 10;
+    } else if (typeof input === "boolean") {
+        return !input
+    }
+};
+
+/*
+//* gpt show me the overload version for problem-1. This only for myself improvement. 
+function formatValue(input: string): string;
+function formatValue(input: number): number;
+function formatValue(input: boolean): boolean;
+
+function formatValue(input: string | number | boolean) {
+    if (typeof input === "string") return input.toUpperCase();
+    if (typeof input === "number") return input * 10;
+    if (typeof input === "boolean") return !input;
+}
+*/
